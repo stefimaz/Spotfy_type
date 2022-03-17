5d0ee38c397d8895deb51e753db486955d93a4f6
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-03-17 17:06:28 -0400

    update files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Album from './pages/Album';
 import './App.css';
+import { Layout } from 'antd';
+import Spotify from "./images/Spotify.png";
+import { SearchOutlined, DownCircleOutlined } from '@ant-design/icons';
+
+const { Footer, Sider, Content } = Layout;
 
 const App = () => {
-  return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/album" element={<Album />} />
-    </Routes>
+  return (
+
+    <Layout>
+      <Sider width={300} className="sideBar">
+        <img src={Spotify} alt="Logo" className="logo"></img>
+        <div className='searchBar'>
+          <spam> Search </spam>
+          <SearchOutlined style={{ frontsize: "30px" }} />
+        </div>
+        <Link to="/">
+          <p style={{ coloor: "#1DB954" }}> Home </p>
+        </Link>
+        <p> Your Music </p>
+        <div className='recentPlayed'>
+          <p className='recentTitle'> RECENTLY PLAYED </p>
+          <div className='install'>
+            <spam> Install App</spam>
+            <DownCircleOutlined style={{ fontSize: "30px" }} />
+          </div>
+        </div>
+      </Sider>
+      <Content className='ContentWindow'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/album" element={<Album />} />
+        </Routes>
+      </Content>
+    </Layout>
+
   )
 };
 
